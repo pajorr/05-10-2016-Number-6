2c60e8e585627fd4f7f056cc71082f18916d5374
Dylan <pajorr2@gmail.com>
2016-10-05 12:07:23 +0700

    Add files via upload

---
+++ number 6 dylan.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+//dylan
+using namespace std;
+
+//functions
+void arrSelectSort(int*[], int);
+void showArray(const int [], int);
+void showArrPtr(int *[], int); 
+
+int main()
+{
+	  int NUM_DONATIONS = 0;   // Number of donations 
+	  cout<<"enter amount of donation set i.e 15 donations : ";
+	  cin>> NUM_DONATIONS;
+	  
+	int donations[NUM_DONATIONS]; 
+									 
+	int *arrPtr[NUM_DONATIONS] = { 0, 0, 0, 0, 0,                               
+	                               0, 0, 0, 0, 0,                                 
+	                               0, 0, 0, 0, 0 };   
+								   
+	 int donate;                 
+	 for (donate = 0; donate < NUM_DONATIONS; donate++)
+	 {
+	 cout << "Enter the donation"
+	 << (donate + 1) << ": ";cin >> donations[donate];
+	 }  							   								 
+
+
+    for (int count = 0; count < NUM_DONATIONS; count++)            
+      arrPtr[count] = &donations[count];
+      
+    arrSelectSort(arrPtr, NUM_DONATIONS); 
+      
+    cout << "The donations, sorted in ascending order are: \n";
+	showArrPtr(arrPtr, NUM_DONATIONS);   
+	
+	cout << "The donations, in their original order are: \n";
+	showArray(donations, NUM_DONATIONS);
+	return 0;
+	}   
+	
+	 void arrSelectSort(int *arr[], int size)
+	 { int startScan, minIndex;
+	 int *minElem;
+	 
+	 for (startScan = 0; startScan < (size - 1); startScan++)
+	 {
+	 minIndex = startScan;
+	 minElem = arr[startScan];
+	 for(int index = startScan + 1; index < size; index++)
+	 {
+	 if (*(arr[index]) < *minElem)
+	 {
+	 minElem = arr[index];
+	 minIndex = index;
+	 }
+	 }
+	 arr[minIndex] = arr[startScan];
+	 arr[startScan] = minElem;
+	 }
+	 } 
+	 
+	  void showArray(const int arr[], int size)
+	  {
+	   for (int count = 0; count < size; count++)
+	    cout << arr[count] << " ";
+		cout << endl;
+		}  
+		
+		 void showArrPtr(int *arr[], int size)
+		 {
+		 for (int count = 0; count < size; count++)
+		 cout << *(arr[count]) << " ";
+		 cout << endl;
+		 }    
+
